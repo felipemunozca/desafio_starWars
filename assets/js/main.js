74e039e3872b6c8160490a16dc4e72b476e994a6
Felipe Muñoz <analista.felipe.m@gmail.com>
2023-06-16 02:35:01 -0400

    Se completa la primera seccion.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 /*** Variables. ***/
+const rowPopulares = document.querySelector('#rowPopulares');
 let perPopulares = [1, 2, 3, 4, 5];
 let perSecundarios = [6, 7, 8, 9, 10, 11];
 let perSignificativos = [12, 13, 14, 15, 16, 18];
@@ -8,6 +9,7 @@ let indexSection1 = perPopulares[0];
 
 /*** Eventos. ***/
 $('#cardPersonajesPopulares').mouseenter(() => {
+
     if (perPopulares.find((popular) => popular == indexSection1)) {
         let personaje = genPersonajesSection1.next().value;
         obtenerDataApi(personaje);
@@ -24,12 +26,44 @@ const obtenerDataApi = (id) => {
     promesa 
         .then(respuesta => respuesta.json())
         .then((datos) => {
-            console.log(datos)
+            // console.log(datos)
+            imprimirCard(datos);
         })
         .catch(error => {
             console.log(error)
         })
 };
+
+const imprimirCard = (datos) => {
+    const {name, height, mass} = datos;
+
+    // console.log("El nombre es: "+ name);
+    // console.log("altura " + height);
+    // console.log("peso " + mass);
+
+    const div1 = document.createElement('div');
+    div1.setAttribute("class", "col-12 col-md-6 col-lg-4");
+    
+    const div2 = document.createElement('div');
+    div2.setAttribute("class", "single-timeline-content d-flex background-show");
+    
+    const icon = document.createElement('i');
+    icon.setAttribute("class", "timeline-icon timeline-icon-red bi bi-circle-fill");
+    
+    const div3 = document.createElement('div');
+    div3.setAttribute("class", "timeline-text");
+
+    const h6 = document.createElement('h6');
+    h6.innerHTML = `${name}`;
+
+    const p = document.createElement('p');
+    p.innerHTML = `Altura: ${height} cm. Peso: ${mass} kg.`;
+
+    rowPopulares.appendChild(div1);
+    div1.appendChild(div2);
+    div2.append(icon, div3);
+    div3.append(h6, p);
+}
 
 //obtenerDataApi();
 
