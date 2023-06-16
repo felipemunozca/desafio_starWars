896a8974b8a33c24db681053a40083bb7c95fa24
Felipe Muñoz <analista.felipe.m@gmail.com>
2023-06-15 20:16:54 -0400

    Se imprime por consola las consultas a la API al hacer mouseenter en la primera seccion.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,50 @@
-console.log('comienza el desafío...')
+/*** Variables. ***/
+let perPopulares = [1, 2, 3, 4, 5];
+let perSecundarios = [6, 7, 8, 9, 10, 11];
+let perSignificativos = [12, 13, 14, 15, 16, 18];
+let genPersonajesSection1 = generadorSeccion1();
+let indexSection1 = perPopulares[0];
+
+
+/*** Eventos. ***/
+$('#cardPersonajesPopulares').mouseenter(() => {
+    if (perPopulares.find((popular) => popular == indexSection1)) {
+        let personaje = genPersonajesSection1.next().value;
+        obtenerDataApi(personaje);
+        indexSection1++;
+        console.log("personaje: " + personaje);
+        console.log("contador: " + indexSection1);
+    }
+});
+
+/*** Funciones. ***/
+const obtenerDataApi = (id) => {
+    let promesa = fetch(`https://swapi.dev/api/people/${id}`);
+
+    promesa 
+        .then(respuesta => respuesta.json())
+        .then((datos) => {
+            console.log(datos)
+        })
+        .catch(error => {
+            console.log(error)
+        })
+};
+
+//obtenerDataApi();
+
+
+/* Funciones generadoras. */
+function* generadorSeccion1() {
+    let i = 0;
+    yield perPopulares[i];
+    i++;
+    yield perPopulares[i];
+    i++;
+    yield perPopulares[i];
+    i++;
+    yield perPopulares[i];
+    i++;
+    yield perPopulares[i];
+    i++;
+}
